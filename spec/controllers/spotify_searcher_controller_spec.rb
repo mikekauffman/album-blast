e6cd3b53bb22b74225b149e4a975c8a9ca636288
mikekauffman <mike.a.kauffman@gmail.com>
2014-05-07 11:14:10 -0600

    added a test to check that url path changes when a search term is entered on the hompeage

--- spec/controllers/spotify_searcher_controller_spec.rb
+++ spec/controllers/spotify_searcher_controller_spec.rb
@@ -4,7 +4,11 @@ feature 'User interacts with the app' do
 
   scenario 'User can search for an artist or band name' do
 
+    visit '/'
+    fill_in 'search_term', with: 'In Like Lions'
+    click_on 'Blast!'
 
+    current_url.should match 'http://www.example.com/In%20Like%20Lions'
 
   end
 
